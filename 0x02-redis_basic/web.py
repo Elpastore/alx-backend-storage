c0846f20902b3b9bae8d85f978fee5c511f6a68c
elpastore <abdoulayesadio1997@gmail.com>
2024-05-15 20:28:07 +0000

    task5 backup

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -4,6 +4,7 @@ web module
 """
 import redis
 import requests
+from datetime import timedelta
 from functools import wraps
 from typing import Callable
 
@@ -12,39 +13,37 @@ r = redis.Redis()
 
 def count_decorator(method: Callable) -> Callable:
     """
-    count_decorator decorator that tracks URL access count and caches results.
+    count_decorator decorator and returns a Callable
     """
-    @wraps(method)
     def wrapper(url: str) -> str:
         """
-        Wrapper function for counting and caching.
+        wrapper function
         """
         key = f'cached:{url}'
         data = r.get(key)
         if data:
-            return data.decode("utf-8")
+            return data.decode(key)
 
         count = f'count:{url}'
-        try:
-            page = method(url)
-            r.incr(count)
-            r.setex(key, 10, page)
-            return page
-        except requests.RequestException as e:
-            print(f"Error fetching page: {e}")
-            return f"Error fetching page: {e}"
+        page = method(url)
+
+        r.incr(count)
+        r.set(key, page)
+        r.expire(key, 10)
+
+        return page
     return wrapper
 
 
 @count_decorator
 def get_page(url: str) -> str:
     """
-    Fetches the HTML content of a given URL using requests.
+    It uses the requests module to obtain the HTML
+    content of a particular URL and returns it.
     """
-    response = requests.get(url)
-    response.raise_for_status()
-    return response.text
+    request = requests.get(url)
+    return request.text
 
 
 if __name__ == '__main__':
-    print(get_page('http://slowwly.robertomurray.co.uk'))
+    get_page('http://slowwly.robertomurray.co.uk')
