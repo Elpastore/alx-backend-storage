ca52118c9dc8743005f2261acbe20d2fd8d5abc5
elpastore <abdoulayesadio1997@gmail.com>
2024-05-15 20:31:15 +0000

    backup

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,32 +1,49 @@
 #!/usr/bin/env python3
-'''A module with tools for request caching and tracking.
-'''
-
-import requests
+"""
+web module
+"""
 import redis
-import time
+import requests
+from datetime import timedelta
+from functools import wraps
+from typing import Callable
 
+r = redis.Redis()
+
+
+def count_decorator(method: Callable) -> Callable:
+    """
+    count_decorator decorator and returns a Callable
+    """
+    def wrapper(url: str) -> str:
+        """
+        wrapper function
+        """
+        key = f'cached:{url}'
+        data = r.get(key)
+        if data:
+            return data.decode(key)
+
+        count = f'count:{url}'
+        page = method(url)
+
+        r.incr(count)
+        r.set(key, page)
+        r.expire(key, 10)
+
+        return page
+    return wrapper
+
+
+@count_decorator
 def get_page(url: str) -> str:
-    # Initialize Redis connection
-    r = redis.Redis()
+    """
+    It uses the requests module to obtain the HTML
+    content of a particular URL and returns it.
+    """
+    request = requests.get(url)
+    return request.text
 
-    # Increment access count for the URL
-    url_count_key = f"count:{url}"
-    r.incr(url_count_key)
 
-    # Check if the cached content exists
-    cached_content = r.get(url)
-    if cached_content:
-        return cached_content.decode("utf-8")
-
-    # Fetch the HTML content from the URL
-    response = requests.get(url)
-    if response.status_code == 200:
-        html_content = response.text
-
-        # Cache the HTML content with a timeout of 10 seconds
-        r.setex(url, 10, html_content)
-
-        return html_content
-    else:
-        return f"Failed to fetch page: {url}"
+if __name__ == '__main__':
+    get_page('http://slowwly.robertomurray.co.uk')
